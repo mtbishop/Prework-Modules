6bc04f47bb9a5d139e2024fa189c9148726b52ff
Matthew Bishop <bishopmatthew76@gmail.com>
2020-11-13 16:00:18 -0600

    Prework for boot camp

---
+++ Module 6/Javascript.js
@@ -0,0 +1,18 @@
+
+    document.getElementById("button1").addEventListener("click", function(){
+        document.getElementById("box").style.height = "400px"
+        document.getElementById("box").style.width = "400px"
+    });
+    document.getElementById("button2").addEventListener("click", function(){
+        document.getElementById("box").style.backgroundColor = "Blue"
+    
+    });
+    document.getElementById("button3").addEventListener("click", function(){
+        document.getElementById("box").style.opacity = "33%"
+    });
+    document.getElementById("button4").addEventListener("click", function(){
+        document.getElementById("box").style.backgroundColor = "orange"
+        document.getElementById("box").style.height = "150px"
+        document.getElementById("box").style.width = "150px"
+        document.getElementById("box").style.opacity = "100%"
+    })
